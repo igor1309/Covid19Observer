cecff52f1edd616abb8c21a847a982a9fadb2d88
Igor Malyarov <igor1309@gmail.com>
2020-03-23 12:44:17 +0300

    Initial Commit

---
+++ Covid19Observer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covid19Observer
+//
+//  Created by Igor Malyarov on 23.03.2020.
+//  Copyright © 2020 Igor Malyarov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
